f8b1d66f438b2f7f3a6b8cbe5c471ce965019689
Rafał Mażuchowski <mazuchowski.r@gmail.com>
2019-10-21 17:44:02 +0200

    Aktualizacja: Stage_0.7

--- Stage/src/main/java/kurs/java/fx/controller/StackPaneController.java
+++ Stage/src/main/java/kurs/java/fx/controller/StackPaneController.java
@@ -1,6 +1,7 @@
 package kurs.java.fx.controller;
 
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
@@ -19,16 +20,36 @@ public class StackPaneController {
 	void initialize() {
 		button.setText("Nowa nazwa");
 
-	}
-	
-	@FXML
-	public void onActionButton(ActionEvent e) {
-		System.out.println("To jest metoda onActionButton"+e.getSource());
-	}
-	
-	@FXML
-	public void onMouseEntered(MouseEvent e) {
-		System.out.println("To jest metoda onMouseEntered"+e.getSource());
+		EventHandler<ActionEvent> handler = new EventHandler<ActionEvent>() {
+
+			public void handle(ActionEvent event) {
+				System.out.println("To jest handler");
+			}
+		};
+
+		button.addEventHandler(ActionEvent.ACTION, handler);
+		
+		EventHandler<MouseEvent> mouseHandlerLambda = e -> {
+			System.out.println("To jest handler lambda");
+		
+		};
+		
+/*
+		EventHandler<MouseEvent> mouseHandler = new EventHandler<MouseEvent>() {
+
+			@Override
+			public void handle(MouseEvent event) {
+				System.out.println("To jest mouse handler");
+				
+			}
+		};
+*/
+		button.addEventHandler(MouseEvent.MOUSE_ENTERED, mouseHandlerLambda);//mouseHandler);
+
+		button.setOnAction(e -> {
+			System.out.println("To jest handler setOn");
+			button.removeEventHandler(MouseEvent.MOUSE_ENTERED, mouseHandlerLambda);
+		}); //w ten sposób mo¿emy zdefiniowaæ EVENT tylko raz. Zostanie wykonany tylko ten nadpisany na koñcu
 	}
 
 }
